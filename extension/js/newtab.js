0da1e427fea0e148e196ffc6931ef44a486ef115
Alex Karcher <me@alexkarcher.com>
2015-11-04 16:16:56 -0500

    Fix duplicate call to newtab.js and removed duplicated code
    Title says it all, I also removed some undefined calls at line 193 and 195 to handleData() and fetchWikipediaArticle.handleData()

--- extension/js/newtab.js
+++ extension/js/newtab.js
@@ -138,88 +138,6 @@ var renderGraph = function renderGraph() {
   chart.renderTo("svg#graph");
 };
 
-//=============================================================
-//Begin history parsing code
-
-// Search history to find up to ten links that a user has typed in,
-// and show those links in a popup.
-function buildTypedUrlList() {
-  // To look for history items visited in the last week,
-  // subtract a week of microseconds from the current time.
-  var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
-  var oneWeekAgo = new Date().getTime() - microsecondsPerWeek;
-
-  // Track the number of callbacks from chrome.history.getVisits()
-  // that we expect to get.  When it reaches zero, we have all results.
-  var numRequestsOutstanding = 0;
-
-  chrome.history.search({
-    'text': '', // Return every history item....
-    'startTime': oneWeekAgo // that was accessed less than one week ago.
-  }, function (historyItems) {
-    // For each history item, get details on all visits.
-    for (var i = 0; i < historyItems.length; ++i) {
-      var url = historyItems[i].url;
-      var processVisitsWithUrl = function processVisitsWithUrl(url) {
-        // We need the url of the visited item to process the visit.
-
-        return function (visitItems) {
-          processVisits(url, visitItems);
-        };
-      };
-      chrome.history.getVisits({ url: url }, processVisitsWithUrl(url));
-      numRequestsOutstanding++;
-    }
-    if (!numRequestsOutstanding) {
-      onAllVisitsProcessed();
-    }
-  });
-
-  // Maps URLs to a count of the number of times the user typed that URL into
-  // the omnibox.
-  var urlToCount = {};
-
-  // Callback for chrome.history.getVisits().  Counts the number of
-  // times a user visited a URL by typing the address.
-  var processVisits = function processVisits(url, visitItems) {
-    for (var i = 0, ie = visitItems.length; i < ie; ++i) {
-      // Ignore items unless the user typed the URL.
-
-      if (!urlToCount[url]) {
-        urlToCount[url] = 0;
-      }
-
-      urlToCount[url]++;
-    }
-
-    // If this is the final outstanding call to processVisits(),
-    // then we have the final results.  Use them to build the list
-    // of URLs to show in the popup.
-    if (! --numRequestsOutstanding) {
-      onAllVisitsProcessed();
-    }
-  };
-
-  // This function is called when we have the final list of URls to display.
-  var onAllVisitsProcessed = function onAllVisitsProcessed() {
-    // Get the top scorring urls.
-    var urlArray = [];
-    for (var url in urlToCount) {
-      urlArray.push(url);
-    }
-
-    // Sort the URLs by the number of times the user typed them.
-    urlArray.sort(function (a, b) {
-      return urlToCount[b] - urlToCount[a];
-    });
-
-    console.log(urlArray.slice(0, 10));
-  };
-}
-
-//End History pasing code
-//======================================================================================
-
 var fetchWikipediaArticle = function fetchWikipediaArticle(titleName) {
 
   var wikiArticleLink = "http://en.wikipedia.org/wiki/" + titleName;
@@ -262,23 +180,23 @@ var fetchWikipediaArticle = function fetchWikipediaArticle(titleName) {
   /****
   var wikiArticleLink = "http://en.wikipedia.org/wiki/Invasion_of_Normandy";
   var handleData = function(data){
-     console.log(data.summary);
+      console.log(data.summary);
     console.log(data.summary.title);
-     $("#title").html($("#title").html() + "" + data.summary.title);
-     console.log(data.summary.summary);
+      $("#title").html($("#title").html() + "" + data.summary.title);
+      console.log(data.summary.summary);
     $("#summary").html($("#summary").html() + "" + data.summary.summary.substring(0,150) + "...");
-     console.log(data.summary.image);
+      console.log(data.summary.image);
     //$("#image").html($("#image").html() + "" + data.summary.image);
     $("#put-image-here").attr("src", imageLink(data));
     $("#image-link").attr("href", wikiArticleLink);
   }
-   function imageLink(data){
+    function imageLink(data){
     return data.summary.image;
   }
-   var handleError = function handleError(error){
+    var handleError = function handleError(error){
     console.log(error);
   }
-   */
+    */
 };
 
 var renderDomainList = function renderDomainList(domains, renderTargetSelector) {
